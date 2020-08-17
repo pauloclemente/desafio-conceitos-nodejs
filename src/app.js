4e400b109c5437e13d9a94b8ac5d650889e5adcd
Paulo Barros <clementedbarros@gmail.com>
2020-08-17 19:23:29 -0300

    apply nodejs crud concepts

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -12,22 +13,63 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const { title, url, techs } = request.body;
+
+  const project = { id: uuid(), title, url, techs, likes: 0};
+
+  repositories.push(project);
+
+  return response.json(project);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  const { title, url, techs} = request.body;
+
+  const { id } = request.params;
+
+  let repository = repositories.find( rep => rep.id === id)
+
+  if(!repository){
+    return response.status(400).json({ message: "This repository was not found"})
+  }
+  
+  repository = {...repository, title, url, techs};
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex( rep => rep.id === id)
+  if (index < 0) {
+    return response.status(400).json({ message: "This repository was not found"})
+  }
+  repositories.splice(index,1);
+  return response.status(204).send;
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find( rep => rep.id === id) 
+  if(!repository){
+    return response.status(400).json({ message: "This repository was not found"})
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
